401ea7ce978e8b0e2d1b1a37fd69cde74810d9e3
Shuma Yoshioka <s64.stdio@gmail.com>
2017-07-28 14:52:21 +0900

    Create HorizontalPositionNavigationBarInsetFrameLayout

---
+++ insetviews/src/main/java/jp/s64/android/insetviews/HorizontalPositionNavigationBarInsetFrameLayout.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 Shuma Yoshioka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.s64.android.insetviews;
+
+import android.content.Context;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import jp.s64.android.insetviews.util.NavigationBarUtils;
+
+public class HorizontalPositionNavigationBarInsetFrameLayout extends AbsHorizontalPositionNavigationBarInsetFrameLayout {
+
+    public HorizontalPositionNavigationBarInsetFrameLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public HorizontalPositionNavigationBarInsetFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public HorizontalPositionNavigationBarInsetFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected boolean isSupportedSide(NavigationBarUtils.NavigationBarPosition position) {
+        return NavigationBarUtils.isHorizontalPosition(position);
+    }
+
+}
