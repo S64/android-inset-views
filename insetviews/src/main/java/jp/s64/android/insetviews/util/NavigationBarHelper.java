f5d5c8b544b6606bf3913810a109036ab6df3b82
Shuma Yoshioka <s64.stdio@gmail.com>
2017-08-01 20:24:36 +0900

    Update NavigationBarHelper

--- insetviews/src/main/java/jp/s64/android/insetviews/util/NavigationBarHelper.java
+++ insetviews/src/main/java/jp/s64/android/insetviews/util/NavigationBarHelper.java
@@ -16,7 +16,11 @@
 
 package jp.s64.android.insetviews.util;
 
+import android.support.annotation.Nullable;
 import android.view.View;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class NavigationBarHelper<SELF extends View & NavigationBarHelper.INavigationBarView> {
 
@@ -28,6 +32,11 @@ public class NavigationBarHelper<SELF extends View & NavigationBarHelper.INaviga
     private static final String RES_NAVIGATION_BAR_ENABLED_DEFTYPE = "bool";
     private static final String RES_NAVIGATION_BAR_ENABLED_DEFPACKAGE = "android";
 
+    private static final String SYSTEM_PROP_QEMU_MAINKEY_DISABLED_KEY = "qemu.hw.mainkeys";
+
+    private static final String SYSTEM_PROP_CLASS_NAME = "android.os.SystemProperties";
+    private static final String SYSTEM_PROP_CLASS_GET_METHOD_NAME = "get";
+
     private final SELF self;
 
     protected NavigationBarHelper(SELF self) {
@@ -35,16 +44,55 @@ public class NavigationBarHelper<SELF extends View & NavigationBarHelper.INaviga
     }
 
     public boolean hasNavigationBar() {
-        int resId = self.getResources().getIdentifier(RES_NAVIGATION_BAR_ENABLED_NAME, RES_NAVIGATION_BAR_ENABLED_DEFTYPE, RES_NAVIGATION_BAR_ENABLED_DEFPACKAGE);
-        if (resId != 0) {
-            return self.getResources().getBoolean(resId);
+        String qemuMainKey = getSystemProperty(SYSTEM_PROP_QEMU_MAINKEY_DISABLED_KEY);
+        if (qemuMainKey != null && qemuMainKey.equals("1")) {
+            return false;
+        } else {
+            int resId = self.getResources().getIdentifier(RES_NAVIGATION_BAR_ENABLED_NAME, RES_NAVIGATION_BAR_ENABLED_DEFTYPE, RES_NAVIGATION_BAR_ENABLED_DEFPACKAGE);
+            if (resId != 0) {
+                return self.getResources().getBoolean(resId);
+            }
         }
-        return true;
+        return false;
+    }
+
+    protected Class<?> getSystemPropertyClass() {
+        try {
+            return self.getContext().getClassLoader().loadClass(SYSTEM_PROP_CLASS_NAME);
+        } catch (ClassNotFoundException e) {
+            throw new NavigationBarHelperException(e);
+        }
+    }
+
+    @Nullable
+    protected String getSystemProperty(String key) {
+        try {
+            Method method = getSystemPropertyClass().getDeclaredMethod(SYSTEM_PROP_CLASS_GET_METHOD_NAME, String.class);
+            Object ret = method.invoke(null, key);
+            if (ret != null) {
+                return (String) ret;
+            }
+            return null;
+        } catch (NoSuchMethodException e) {
+            throw new NavigationBarHelperException(e);
+        } catch (InvocationTargetException e) {
+            throw new NavigationBarHelperException(e);
+        } catch (IllegalAccessException e) {
+            throw new NavigationBarHelperException(e);
+        }
     }
 
     public interface INavigationBarView {
 
         void setZeroHeightIfNavigationBarDisabled(boolean doZeroHeight);
+
+    }
+
+    public static class NavigationBarHelperException extends RuntimeException {
+
+        public NavigationBarHelperException(Throwable throwable) {
+            super(throwable);
+        }
 
     }
 
